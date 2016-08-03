c20c5a4ad3c591b6c8a5c3e15344859e06049184
McN33ly <rmcneely23@gmail.com>
2016-08-03 09:40:32 -0400

    Fixed jQuery buttons to pull correctly, deleted extraneous artifact code.

--- app/assets/javascripts/assessment-js.js
+++ app/assets/javascripts/assessment-js.js
@@ -9,19 +9,17 @@ $('button.load-assessment').on('click', function(e){
       return 'prev'
     }
   }
-debugger
   var getNewAssessment = $.get("/assessment-"+ newAssessment(e)  + ".json", {'data-id': $(e.target).attr('data-id'), 'format': 'json'}).done( function(data){
-    var datum = data
-    debugger
-      $('h1#assessment_name').html(data.name)
-      $('h4 a#assessment_by').text(data.user.name)
-      $('h4 a#assessment_by').attr("href", '/users/' + data.user.id  )
-      $('p a#repo_link').attr("href", data.link)
-      $('button.buttons').attr('data-id', data.id)
-      $('#assessment_skills_array span').text(JSON.stringify(data.skills))
-      var changeList = $('ul#assessment_skills_array').html(data.skills)
+    var datum = data[0]
+      $('h1#assessment_name').html(datum.name)
+      $('h4 a#assessment_by').text(datum.user.name)
+      $('h4 a#assessment_by').attr("href", '/users/' + datum.user.id  )
+      $('p a#repo_link').attr("href", datum.link)
+      $('button.load-assessment').attr('data-id', datum.id)
+      $('#assessment_skills_array span').text(JSON.stringify(datum.skills))
+      var changeList = $('ul#assessment_skills_array').html(datum.skills)
       // debugger
-      data.skills.forEach(function(skill){
+      datum.skills.forEach(function(skill){
       $('ul#assessment_skills_array').append('<li>'+ skill.name + '</li>')
     });
   });
@@ -31,25 +29,3 @@ debugger
     alert("Woops! Something went wrong")
   });
 });
-// $('button.load_assessment').on('click', function(e){
-//   e.preventDefault()
-//   var assessment = parseInt($(this).attr('data-id')) - 1
-//   // debugger
-//   var getNext = $.get("/assessment/"+ assessment  + ".json", function(data){
-//       $('div.change_assessment_to_render p').text(JSON.stringify(data))
-//       $('h1#assessment_name').html(data.name)
-//       $('h4 a#assessment_by').text(data.user.name)
-//       $('h4 a#assessment_by').attr("href", '/users/' + data.user.id  )
-//       $('p a#repo_link').attr("href", data.link)
-//       $('button.buttons').attr('data-id', data.id)
-//       $('#assessment_skills_array span').text(JSON.stringify(data.skills))
-//       var changeList = $('ul#assessment_skills_array').html(data.skills)
-//       // debugger
-//       data.skills.forEach(function(skill){
-//       $('ul#assessment_skills_array').append('<li>'+ skill.name + '</li>')
-//     });
-//   });
-//   getNext.error(function(){
-//     alert("Woah there looks like you're at the first one already!")
-//   });
-// });
